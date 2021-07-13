e0ebb1b7cdaa98d8f523f71d9452887f74134522
Sunghyun Cho <mail@chosunghyun.com>
2021-07-13 17:58:41 +0900

    add border & center el

--- pages/tailwind-docs/chatbubble.tsx
+++ pages/tailwind-docs/chatbubble.tsx
@@ -2,18 +2,26 @@ import Image from 'next/image'
 import Favicon from '../../public/favicon.ico'
 
 const chatbubble = () => (
-  <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
-    <div className='flex-shrink-0 relative'>
-      <div className='h-12 w-12'>
-        <Image layout='fill' src={Favicon} alt='Favicon' />
-      </div>
-    </div>
-    <div>
-      <div>
-        <div className='text-xl font-medium text-black'>
-          Chat Bubble
+  <div className='flex h-screen'>
+    <div className='p-6 max-w-sm m-auto bg-white rounded-xl shadow-md flex items-center space-x-4 border-2'>
+      <div className='flex-shrink-0 relative'>
+        <div className='h-12 w-12'>
+          <Image
+            layout='fill'
+            src={Favicon}
+            alt='Favicon'
+          />
         </div>
-        <p className='text-gray-500'>You have a message.</p>
+      </div>
+      <div>
+        <div>
+          <div className='text-xl font-medium text-black'>
+            Chat Bubble
+          </div>
+          <p className='text-gray-500'>
+            You have a message.
+          </p>
+        </div>
       </div>
     </div>
   </div>
