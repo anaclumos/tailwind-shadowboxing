0cb6306f94c09f0f8e8bbb10b13d1fc2873487dd
Sunghyun Cho <mail@chosunghyun.com>
2021-07-14 00:37:08 +0900

    design tweaks

--- pages/tailwind-docs/user-email-form.tsx
+++ pages/tailwind-docs/user-email-form.tsx
@@ -5,7 +5,7 @@ const UserEmailForm = () => {
   return (
     <div className='flex h-screen p-6 bg-green-100'>
       <div className='m-auto space-y-3'>
-        <div className='w-full px-6 py-5 mx-auto space-y-1 overflow-hidden transition duration-500 transform border-2 border-green-500 border-opacity-25 rounded-lg cursor-pointer select-none hover:border-2 group hover:bg-green-100 hover:shadow-lg motion-reduce:transform-none hover:scale-105 click:bg-green-700'>
+        <div className='w-full px-6 py-5 mx-auto space-y-1 overflow-hidden transition duration-500 transform border-2 border-green-500 border-opacity-25 rounded-lg cursor-pointer select-none hover:border-2 group hover:shadow-lg motion-reduce:transform-none hover:scale-105'>
           <p className='text-lg font-semibold text-green-600'>
             New Project
           </p>
@@ -19,12 +19,12 @@ const UserEmailForm = () => {
           onSubmit={signupUser}
         >
           <input
-            className='flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 transition duration-500 transform bg-white border border-transparent rounded-lg shadow-md appearance-none focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent motion-reduce:transform-none hover:scale-105'
+            className='flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 transition duration-500 transform bg-white border-2 border-green-500 border-opacity-25 rounded-lg appearance-none hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent motion-reduce:transform-none hover:scale-105'
             placeholder='Your Email'
             type='email'
           />
           <button
-            className='flex-shrink-0 px-4 py-2 text-base font-semibold text-white transition duration-500 transform bg-green-600 rounded-lg shadow-md focus:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-200 motion-reduce:transform-none hover:scale-105 tramsform '
+            className='flex-shrink-0 px-4 py-2 text-base font-semibold text-white transition duration-500 transform bg-green-600 rounded-lg hover:shadow-lg focus:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-200 motion-reduce:transform-none hover:scale-105 tramsform'
             type='button'
           >
             Sign up
