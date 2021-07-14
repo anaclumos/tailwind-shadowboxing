39ed43f0079645686a1321022b5820b3a5f9f828
Sunghyun Cho <mail@chosunghyun.com>
2021-07-14 15:19:12 +0900

    do not obfuscate styles

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const MangleCssClassPlugin = require('mangle-css-class-webpack-plugin')
-
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cataas.com'],
   },
-  plugins: [
-    new MangleCssClassPlugin({
-      classNameRegExp: '[cl]-[a-z][a-zA-Z0-9_]*',
-      log: true,
-    }),
-  ],
 }
